76f7ec29919d13d3bf68e42634328bd4dc3e47c6
maratz <maratz375@gmail.com>
2021-10-14 09:22:29 +0500

    добавил новую строку

--- lesson01/lesson01_task01.py
+++ lesson01/lesson01_task01.py
@@ -1,2 +1,8 @@
 # Test Pycharm - GitHub
 print('Lesson 01. Task 01')
+
+# Add in new_branch
+print('Редактирование файла в ветке new_branch')
+
+# добавил новую строку
+print('добавил новую строку')
